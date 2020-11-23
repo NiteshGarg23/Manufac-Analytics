75f3fcce2b72e61509c3e59ab1ddae98dc23958b
Nitesh Garg <47839824+NiteshGarg23@users.noreply.github.com>
2020-11-23 08:19:07 +0530

    Update Home.js

--- manufac-analytics/src/components/home/Home.js
+++ manufac-analytics/src/components/home/Home.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { useHistory } from "react-router";
+import Button from '@material-ui/core/Button';
+import Image from '../../assets/background.png'
 
 function Home(){
 
@@ -12,11 +14,39 @@ function Home(){
         history.replace("/login");
     }
 
+    const styles = {
+        background: {
+            backgroundImage: `url(${Image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh'
+        }
+    }
+
     return(
-        <div>
-            <h1>Welcome {username}</h1>
-            <br />
-            <p onClick={handleLogout}>Logout</p>
+        <div style={styles.background}>
+            <header class="mui-appbar mui--z1">
+                <div class="mui-container">
+                    <table>
+                        <tr class="mui--appbar-height">
+                        <td class="mui--text-title"><h2>Hello {username}</h2></td>
+                        </tr>
+                    </table>
+                </div>
+            </header>
+            <div class="mui--text-center">
+                <h1>Welcome to Manufac Analytics!</h1>
+                <br />
+                <Button 
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    onClick={handleLogout}
+                >
+                    Logout 
+                </Button>
+            </div>
         </div>
     )
 }
